919712d54f33f9ae4820d176a73bfdc86c8160c7
Erick Mathus <erick.mathus@yahoo.com>
2019-04-30 13:47:56 -0400

    adding click hide element

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 let x = document.getElementById('profileBtn')
-let y = document.getElementsByClassName('para')
-console.log(y)
-
-let main = prompt('Who is your main?') // ask question, store answer in main variable
-console.log(main) // log main variable to the console
-
-//main variable is input from user. Which means it could look like anything.
-
-if (main.toLowerCase() == 'roy') { // convert string in main to ensure it will be lower
-  alert('That is correct!')
-}else{
-  alert('You suck!')
+let paras = document.getElementsByClassName('para')
+// function functionName(){}
+function hideElement(){
+  paras[3].classList.add('hidden')
 }
+
+// thisIsCalled CamelCase
+// this_is_called snake_case
+
+//takes two arguments: 1. the event 2.The function to run when event happened
+paras[0].addEventListener('click', hideElement)
