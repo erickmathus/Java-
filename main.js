b368d0afadd0c5f45498a06a6d640280ae18a631
Erick Mathus <erick.mathus@yahoo.com>
2019-04-30 13:36:34 -0400

    examples of dialog log question

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-document.write('Erick')
+let x = document.getElementById('profileBtn')
+let y = document.getElementsByClassName('para')
+console.log(y)
+
+let main = prompt('Who is your main?') // ask question, store answer in main variable
+console.log(main) // log main variable to the console
+
+//main variable is input from user. Which means it could look like anything.
+
+if (main.toLowerCase() == 'roy') { // convert string in main to ensure it will be lower
+  alert('That is correct!')
+}else{
+  alert('You suck!')
+}
