3cf5d97e131d77f6d6150b5bae3a74dd4a510e55
Erick Mathus <erick.mathus@yahoo.com>
2019-04-30 16:15:29 -0400

    refined and added deposit history

--- app/main.js
+++ app/main.js
@@ -4,15 +4,23 @@ function depositMoney() {
   let currentBal = bal.innerText
   let total = parseInt(currentBal) + parseInt(amount)
   bal.innerText = total
-
+  let history = document.querySelector('#history')
+  history.innerHTML += '<p>You deposited ' + amount + '</p>'
 }
 function withdrawMoney(){
-  let amount = prompt('How much do you want to deposit?')
+  let amount = prompt('How much do you want to withdraw?')
   let history = document.querySelector('#history')
+  let OD = document.querySelector('#odProtect')
   let bal = document.querySelector('#bank-balance')
   let currentBal = bal.innerText
   let total = parseInt(currentBal) - parseInt(amount)
+  if (OD.checked() == true){
+    alert('Overdraft Prevented!')
+    let total = currentBal
+  } else{
+    let total = parseInt(currentBal) - parseInt(amount)
   bal.innerText = total
+}
   if (total <= 0) {
     bal.classList.add('negative-balance')
   }
